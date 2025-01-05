06686dda5e69c06bdc5c3f334b4fe45f26559ee1
yaroslavlyashenko <lyashenkoyaroslav14102000@gmail.com>
2025-01-05 15:06:25 +0100

    3.3

--- 2.1.py
+++ 2.1.py
@@ -1,4 +1,15 @@
-list = [1,2,3,4,5,6,7]
-list.insert(0, 7)
-list.pop(7)
-print(list)
+lst = [1,2,3,4,5,6]
+a = lst[0:3]
+b = lst[3:6]
+print([a , b])
+
+lst = [1,2,3,4,5,6,7]
+a = lst[0:4]
+b = lst[4:7]
+print([a , b])
+
+lst = []
+a = lst
+b = lst
+print([a,b])
+
